81fa5dc15bdbd2be6df00589e4c9b22c021d38d1
Sucik MAnny <sucik@outlook.com>
2020-11-07 19:56:55 +0000

    completed week 6 blog

--- intro_pro/python_lessons/loops.py
+++ intro_pro/python_lessons/loops.py
@@ -5,3 +5,15 @@ for x in nums:
 name = 'sucik'
 if name == 'sucik':
   print('yeah baby i like that')
+
+num = list(range(1, 13))
+for x in num:
+    if x == 6:
+        print(f'we are half way there {x}')
+        continue
+    elif x == 12:
+        print(f'this is the end number {x}')
+        continue
+    print(x)
+
+
