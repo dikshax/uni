d3280008250d259cce1a35751620f510e4771792
Sucik MAnny <sucik@outlook.com>
2020-11-12 03:01:46 +0000

    completed part-3

---
+++ intro_pro/workshop_5/part_4.py
@@ -0,0 +1,5 @@
+# 1. create a program taht picks a winner for a contest or prize draw. The program shoudl prompt for the names of the entrants and their contant details( address, mobile no ets ) until the user enters a black string. then select a winner at random and print their details. this is showcased in the demo program called coin_change included in last week's sample sode.
+# b. add a text based menu to the program with two initial options:
+    # add contentant or quit with the relevant functions.
+# c. add menu option that aloows users to view the current list of contets.
+# d. add a menu option that allows users to delete contestants.
