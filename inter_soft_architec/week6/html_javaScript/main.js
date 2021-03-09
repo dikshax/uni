7cdf6e42eda5cda69e2f1f1362bceffe350fae51
sucik <sucik@outlook.com>
2021-03-09 04:24:41 +0000

    some more stuff for internet-saftware architechture

--- inter_soft_architec/week6/html_javaScript/main.js
+++ inter_soft_architec/week6/html_javaScript/main.js
@@ -1,19 +1,19 @@
- fetch('https://api.openweathermap.org/data/2.5/weather?q=Sheffield,uk&appid=a9674e7298c06e2d719d1133e6178cba&units=metric')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Sheffield,uk&appid=a9674e7298c06e2d719d1133e6178cba&units=metric')
 
         .then(response => response.json())
         .then(response => {
             // display whole API response in brower console (take a look at it!)
         console.log(response);
 
-        // including city name
+        // including city name 
         document.querySelector('#city').innerHTML = response.name;
 
         // including the country name
         document.querySelector('#country').innerHTML = response.sys.country;
-
+        
         // getting the icon
         document.querySelector('#pic').src = 'http://openweathermap.org/img/wn/' + response.weather[0].icon + '@4x.png';
-
+        
         // getting the tempreture
         document.querySelector('#tempreture').innerHTML = Math.round(response.main.temp) + '\u00B0';
 
@@ -22,7 +22,7 @@
 
         // getting the low  tempreture
         document.querySelector('#low').innerHTML = 'L: ' + Math.round(response.main.temp_min) + '\u00B0';
-
+        
         })
 
         .catch(err => {
